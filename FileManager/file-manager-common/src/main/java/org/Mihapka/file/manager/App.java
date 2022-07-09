671c2786cfd0c9f89c3628c6b12e79fb7d7cd115
Андрей Александрович Михайлов <mikhaylov.andrey.a@gmail.com>
2022-07-09 20:12:17 +0300

    created structure
    Co-Authored-By: Андрей Александрович Михайлов <mikhaylov.andrey.a@gmail.com>

---
+++ FileManager/file-manager-common/src/main/java/org/Mihapka/file/manager/App.java
@@ -0,0 +1,13 @@
+package org.Mihapka.file.manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
